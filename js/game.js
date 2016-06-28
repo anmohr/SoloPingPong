b32f628ce606ab3dcd6507db585dfce21084947b
Arielle Mohr <anm67@drexel.edu>
2016-06-28 14:48:23 -0400

    Week 02 Changes
    Starting to build solo pong game.

--- js/game.js
+++ js/game.js
@@ -18,5 +18,32 @@ window.cancelRequestAnimFrame = ( function() {
 			window.msCancelRequestAnimationFrame     ||
 		clearTimeout;
 } )();
+//DO NOT TOUCH CODE ABOVE
+//console.log('Holla');
 
-console.log('Holla');
+//STEP 01 .. ANM .. Create game canvas and track mouse position
+var gameCanvas = document.getElementById("canvas");
+//Store HTML5 canvas tag into JS variable
+
+var ctx = gameCanvas.getContext("2d"); // Create context 2D
+var W = window.innerWidth;
+var H = window.innerHeight;
+var mouseObj = {};
+
+gameCanvas.width = W;
+gameCanvas.height = H;
+
+function paintCanvas() {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, W, H);
+}
+
+paintCanvas();
+
+function trackPosition(evt) {
+    mouseObj.x = evt.pageX;
+    mouseObj.y = evt.pageY;
+    console.log("cursor is x :" + mouseObj.x + "cursor y is :" + mouseObj.y);
+}
+
+gameCanvas.addEventListener("mousemove", trackPosition, true);
