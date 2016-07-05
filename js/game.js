a6c6c9820a643368591865d24645e0e5b83006a9
Arielle Mohr <anm67@drexel.edu>
2016-07-05 14:33:37 -0400

    Update main screen
    Need main screen objects.

--- js/game.js
+++ js/game.js
@@ -1,23 +1,23 @@
 // RequestAnimFrame: a browser API for getting smooth animations
-window.requestAnimFrame = (function(){
-	return  window.requestAnimationFrame   ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame    ||
-			window.oRequestAnimationFrame      ||
-			window.msRequestAnimationFrame     ||
-		function( callback ){
-			return window.setTimeout(callback, 1000 / 60);
-		};
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            return window.setTimeout(callback, 1000 / 60);
+        };
 })();
 
-window.cancelRequestAnimFrame = ( function() {
-	return  window.cancelAnimationFrame          ||
-			window.webkitCancelRequestAnimationFrame ||
-			window.mozCancelRequestAnimationFrame    ||
-			window.oCancelRequestAnimationFrame      ||
-			window.msCancelRequestAnimationFrame     ||
-		clearTimeout;
-} )();
+window.cancelRequestAnimFrame = (function () {
+    return window.cancelAnimationFrame ||
+        window.webkitCancelRequestAnimationFrame ||
+        window.mozCancelRequestAnimationFrame ||
+        window.oCancelRequestAnimationFrame ||
+        window.msCancelRequestAnimationFrame ||
+        clearTimeout;
+})();
 //DO NOT TOUCH CODE ABOVE
 //console.log('Holla');
 
@@ -33,6 +33,7 @@ var mouseObj = {};
 gameCanvas.width = W;
 gameCanvas.height = H;
 
+//STEP 02 .. ANM .. Clear page canvas by covering it in black
 function paintCanvas() {
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, W, H);
@@ -47,3 +48,59 @@ function trackPosition(evt) {
 }
 
 gameCanvas.addEventListener("mousemove", trackPosition, true);
+
+//STEP 03 .. ANM .. Place a ball on the canvas
+var ball = {}; //Ball Object
+ball = {
+    x: 50,
+    y: 50,
+    r: 5,
+    c: "#FFFFFF",
+    vx: 4,
+    vy: 8,
+
+    draw: function () {
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        ctx.fill();
+    }
+}
+
+ball.draw();
+
+//STEP 04 .. ANM .. Place a start button on canvas
+var startBtn = {}; //Start button object
+startBtn = {
+    w: 100,
+    h: 50,
+    x: W / 2 - 50,
+    y: H / 2 - 25,
+    
+    draw: function () {
+     ctx.strokeStyle = "#FFFFFF";
+        ctx.lineWidth = "2";
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        
+        ctx.font = "18px Arial, sans-serif";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText("Start", W / 2, H / 2);
+ }
+}
+
+startBtn.draw();
+
+//STEP 05 .. ANM .. Place score and points on canvas
+var points = 0; // game points
+function paintScore(){
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "18px Arial, sans-serif";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Score: " + points, 20, 20);
+    
+}
+
+paintScore();
